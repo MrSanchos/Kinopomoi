f18a92e9bc33c23196888773e15566c10c38f962
Sanchos <loljo789@gmail.com>
2025-10-03 00:18:16 +0500

    v0.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import './App.css'
+import yrna from './assets/yrna.svg';
 
 function App() {
-
+    return (
+        <div className='menu'>
+            <img src={yrna} width="64" height="64"></img>
+            <h1>KINOPOMOI</h1>
+        </div>
+  );
 }
 
 export default App
