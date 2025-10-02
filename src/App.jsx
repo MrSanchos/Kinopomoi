84e8d02a76b9ba2ef43d88595cbe8074b8187628
Sanchos <loljo789@gmail.com>
2025-10-03 01:36:04 +0500

    v0.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,42 @@
 import './App.css'
 import yrna from './assets/yrna.svg';
+import xx from './assets/xx.png';
 
 function App() {
     return (
-        <div className='menu'>
-            <img src={yrna} width="64" height="64"></img>
-            <h1>KINOPOMOI</h1>
+        <div>
+            <div className='menu'>
+                <img src={yrna} width="64" height="64"></img>
+                <h1>KINOPOMOI</h1>
+                <input className='poisk' type="text" />
+                <button className='reg'>РЕГИСТРАЦИ | ВХОД</button>
+            </div>
+            <div className='film'>
+                <img src={xx} width="512" height="288"></img>
+                <img src={xx} width="512" height="288"></img>
+                <img src={xx} width="512" height="288"></img>
+            </div>
+            <div className='film'>
+                <img src={xx} width="512" height="288"></img>
+                <img src={xx} width="512" height="288"></img>
+                <img src={xx} width="512" height="288"></img>
+            </div>
+            <div className='film'>
+                <img src={xx} width="512" height="288"></img>
+                <img src={xx} width="512" height="288"></img>
+                <img src={xx} width="512" height="288"></img>
+            </div>
+            <div className='film'>
+                <img src={xx} width="512" height="288"></img>
+                <img src={xx} width="512" height="288"></img>
+                <img src={xx} width="512" height="288"></img>
+            </div>
+            <div className='film'>
+                <img src={xx} width="512" height="288"></img>
+                <img src={xx} width="512" height="288"></img>
+                <img src={xx} width="512" height="288"></img>
+            </div>
+
         </div>
   );
 }
